f57aa171ec1fe907eab79333149e23ea00144991
echob <echoquette@echo-b.ca>
2016-04-13 12:42:08 -0400

    Mise à jours wp 4.5

--- wp-content/themes/RapportAnnuel/library/admin.php
+++ wp-content/themes/RapportAnnuel/library/admin.php
@@ -22,6 +22,13 @@ Digging into WP - http://digwp.com/2010/10/customize-wordpress-dashboard/
 
 */
 
+// Update CSS within in Admin
+function admin_style() {
+  wp_enqueue_style('admin-styles', get_template_directory_uri().'/library/css/admin.css');
+}
+
+add_action('admin_enqueue_scripts', 'admin_style');
+
 /************* DASHBOARD WIDGETS *****************/
 
 // disable default dashboard widgets
